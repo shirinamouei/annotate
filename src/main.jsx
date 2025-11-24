ba3b2a9b2f2b13abb0092a66e6d2ddde62c391f2
shirinam <shirinam@uw.edu>
2025-11-23 23:52:03 -0800

    Fix router issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import Login from './pages/Login'
 import Annotate from './pages/Annotate'
@@ -8,12 +9,14 @@ import Admin from './pages/Admin'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/annotate" element={<Annotate />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>,
 )
